ac052fa2fecbba95ffa7b186806c90f9e6c2d517
Alexander <youdoit@mail.ru>
2021-02-18 14:42:25 +0600

    Created component for top panel

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { TopPanelComponent } from './top-panel/top-panel.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
+    TopPanelComponent,
   ],
   imports: [
     BrowserModule

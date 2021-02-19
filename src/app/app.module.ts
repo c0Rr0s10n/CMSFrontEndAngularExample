d656f11440230621f33098d3a67c64709c95a530
Alexander <youdoit@mail.ru>
2021-02-20 00:43:49 +0600

    Generated component for user's information

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { TopPanelComponent } from './top-panel/top-panel.component';
+import { UserInfoComponent } from './user-info/user-info.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     TopPanelComponent,
+    UserInfoComponent,
   ],
   imports: [
     BrowserModule

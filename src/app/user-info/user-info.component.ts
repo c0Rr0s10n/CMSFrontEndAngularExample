b058cb442c8c37bca348eaa6cec33d44c54d6491
Alexander <youdoit@mail.ru>
2021-02-23 02:39:59 +0600

    Struggling about animations

--- src/app/user-info/user-info.component.ts
+++ src/app/user-info/user-info.component.ts
@@ -1,3 +1,4 @@
+import { NONE_TYPE } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -14,7 +15,12 @@ export class UserInfoComponent implements OnInit {
 
 
   changeTab(event: any) {
-    let tabs = document.querySelectorAll(".tab");
-    event.srcElement.classList.add("tab-active")
+    if (event.srcElement.tagName == "A"){
+      let activeTab = Array.from(document.getElementsByClassName('mat-form-field-infix') as HTMLCollectionOf<HTMLElement>)
+      activeTab[0].classList.remove("tab-active");
+      activeTab[0].style.animation = "deactivation";
+      event.srcElement.classList.add("tab-active")
+
+    }
   }
 }

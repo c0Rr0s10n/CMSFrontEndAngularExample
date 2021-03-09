62def77b0693aed0390cbadaa4d905b21af03e94
Alex <youdoit@mail.ru>
2021-03-09 15:23:30 +0600

    Continued work on cms

--- src/app/user-info/user-info.component.ts
+++ src/app/user-info/user-info.component.ts
@@ -15,12 +15,14 @@ export class UserInfoComponent implements OnInit {
 
 
   changeTab(event: any) {
-    if (event.srcElement.tagName == "A"){
+    if (event.srcElement.tagName == "A" || event.srcElement.tagName == "LI"){
       let activeTab = Array.from(document.getElementsByClassName('tab-active') as HTMLCollectionOf<HTMLElement>)
       activeTab.forEach(element => {
         element.classList.remove("tab-active");
+        element.classList.add("tab-deactivated");
       });
       event.srcElement.classList.add("tab-active")
+      event.srcElement.classList.remove("tab-deactivated")
 
     }
   }
